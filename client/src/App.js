0e37402a6a67ca3bf96dbe6fbe55490e3822c49f
Simon Devillers <eloise.taurel@student-cs.fr>
2019-06-19 16:15:34 +0200

    add app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,133 @@
-import React from 'react';
+
+
+
+
+import React, { Component }  from 'react';
 import logo from './logo.svg';
+import popcorn from './popcorn2.png';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Redirect, withRouter} from "react-router-dom";
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div className="App">
+      <img src={popcorn} className="App-logo" alt="logo" />
+      <AuthButton />
+        <ul>
+            <h1><Link to="/protected">Ma page</Link></h1>
+        <Route path="/login" component={Login} />
+        <PrivateRoute path="/protected" component={Protected} />
+        <PrivateRoute path="/mapage" component={Home} />
+        <hr />
+      <p>Pas encore inscrit ?</p><Link to="/register">Inscrivez-vous !</Link>
+        </ul>
+
+      </div>
+    </Router>
+  );
+}
+function Mapage() {
+    return <Link to={'/home'}>Ma page</Link>;
+}
+
+function Register() {
+  return (
+    <div>
+      <h2>Inscrivez-vous !</h2>
     </div>
   );
 }
+
+function Protected() {
+  return <Link to={'/page'}>Ma page</Link>;
+}
+
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+
+const AuthButton = withRouter(
+  ({ history }) =>
+    fakeAuth.isAuthenticated ? (
+      <p>
+        Bienvenue !{" "}
+        <button
+          onClick={() => {
+            fakeAuth.signout(() => history.push("/"));
+          }}
+        >
+          Déconnexion
+        </button>
+      </p>
+    ) : (
+      <p> </p>
+    )
+);
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        fakeAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+
+class Login extends Component {
+  state = { redirectToReferrer: false };
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState({ redirectToReferrer: true });
+    });
+
+  };
+
+  render() {
+    let { from } = this.props.location.state || { from: { pathname: "/" } };
+    let { redirectToReferrer } = this.state;
+
+    if (redirectToReferrer) return <Redirect to={from} />;
+
+    return (
+      <div>
+        <p>Vous devez vous connecter pour accéder à votre page</p>
+        <form action="">
+        <div>Identifiant :
+            <input type="text" name="userid"/></div>
+        <div>Mot de passe :
+            <input type="password" name="psw"/></div>
+        </form>
+        <button onClick={this.login}>Connexion</button>
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
