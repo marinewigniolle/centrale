aee1f79c6c89ab0445b3f2bc3d5fca8832331785
marine.wigniolle@student-cs.fr <Benodet29!>
2019-06-19 15:31:22 +0200

    Add Home

---
+++ client/src/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Home.css'
+import Titanic from './Titanic.jpg'
+import MammaMia from './MammaMia.jpg'
+
+function Home() {
+    return (
+        <div>
+        <h1 className = "Rubrics">Recommandés pour vous</h1>
+
+                <div class="Pictures">
+                <MovieTitle title='Titanic' image={Titanic} url="https://www.youtube.com/watch?v=LD8dyWYuX0g&t=2s"/>
+                <MovieTitle title='Mamma Mia!' image={MammaMia} url="https://www.youtube.com/watch?v=w3MOV3cYHqA"/>
+                </div>
+
+         <h1 className = "Rubrics">Les mieux notés</h1>
+
+
+        <h1 className = "Rubrics">À l'affiche </h1>
+
+
+        <h1 className= "Rubrics">Tous les films</h1>
+
+        </div>
+    )
+}
+
+const MovieTitle = (props) => (
+    <div className="movie-title">
+        <img src={props.image}></img>
+        <a
+        href={props.url}>
+        <div>{props.title}</div></a>
+    </div>
+)
+
+export default Home
+
+
